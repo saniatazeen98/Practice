3b8f52ebdd35ef1e0066ae22fa4ef11cc6447608
Majid <majidpathan78@gmail.com>
2021-09-27 20:38:56 +0530

    webpage

--- demo.js
+++ demo.js
@@ -1,4 +1,14 @@
-var a = 10;
+~var a = 10;
 var b = 10;
 
 console.log(a,b);
+<br class="hidden lg:inline-block">We aim to deliver more than what client's expect.
+</h1>
+<p class="mb-8 leading-relaxed">FUTURE HOUSE Architects believes that thoughtful architecture is a pillar of a
+  strong community. Our work locally has enabled us to see how good design can reinvigorate a forgotten space,
+  bring people together, and create positive change. It is our privilege to design new spaces for families,
+  communities, and businesses to call “home”.</p>
+  <p class="lg:w-2/3 mx-auto leading-relaxed text-base">We love to meet new people and talk about possibilities.
+  </p>
+  <a class="text-indigo-500">majidpathan78@gmail.com</a>
+  <br> Hyderabad, Telangana 500049.
